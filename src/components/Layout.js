78e1a649b25fc73bfc4922bacf5a326af91687c8
Ilia Kravchuk <ilia.k.elogic@gmail.com>
2023-06-11 17:05:33 +0300

    Pages, Navbar, Layout

---
+++ src/components/Layout.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Navbar from 'components/Navbar';
+
+const Layout = (props) => {
+    const { children } = props;
+
+    return (
+        <div className="layout">
+            <Navbar/>
+            <div className="content">
+                {children}
+            </div>
+            <footer>
+                <p>© {new Date().getFullYear()} Gatsby Starter</p>
+            </footer>
+        </div>
+    );
+};
+
+export default Layout;
---
+++ src/pages/about.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Layout from 'components/Layout';
+
+const About = () => {
+    return (
+        <Layout>
+            <div>
+                <h1>About Page</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </div>
+        </Layout>
+    );
+};
+
+export default About;
